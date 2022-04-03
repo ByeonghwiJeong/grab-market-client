99740a3914b3b1dc55a4233dc1ebe7c04c7305fc
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-04-03 23:31:44 +0900

    Link태크 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import MainPageComponent from "./main/index";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
